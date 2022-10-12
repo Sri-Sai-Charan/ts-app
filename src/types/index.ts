e02e21c7813550ece811d2ff4e189288587039f5
Sri-Sai-Charan <svellise@umd.edu>
2022-10-12 18:31:01 -0400

    Creating custom interfaces

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+import { AnyAction } from '@reduxjs/toolkit';
+
+export interface ModelInterface {
+    id: string;
+    text: string;
+    isFinished: boolean;
+}
+
+export interface AppInterface {
+    labelText?: string;
+    addHandler: (v: string) => AnyAction;
+    removeHandler: (v: string) => AnyAction;
+    selectorState: ModelInterface[];
+    droppableId: string;
+}
